2cf0a616278c5a51e18a44d64db92c299fe5f484
Vitali Solovchuk <v.solovchuk@softteco.com>
2022-09-08 14:49:38 +0300

    added firestore config to empty angular

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'authangular-8a330',
+    appId: '1:841619353292:web:d84ba57e285e4ccce35c69',
+    storageBucket: 'authangular-8a330.appspot.com',
+    apiKey: 'AIzaSyBG_fIV-v8UA4nh7iMWWM5dxwe5XBZlq0c',
+    authDomain: 'authangular-8a330.firebaseapp.com',
+    messagingSenderId: '841619353292',
+    measurementId: 'G-6YGCZBBVTB',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'authangular-8a330',
+    appId: '1:841619353292:web:d84ba57e285e4ccce35c69',
+    storageBucket: 'authangular-8a330.appspot.com',
+    apiKey: 'AIzaSyBG_fIV-v8UA4nh7iMWWM5dxwe5XBZlq0c',
+    authDomain: 'authangular-8a330.firebaseapp.com',
+    messagingSenderId: '841619353292',
+    measurementId: 'G-6YGCZBBVTB',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
